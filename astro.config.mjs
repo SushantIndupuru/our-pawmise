34d4551c9f3862b086994d29feffc05c886f0cac
Sushant <sushindupuru@gmail.com>
2026-08-03 17:32:57 -0700

    sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        preview: {
-            allowedHosts: ['localhost'],
-        }
-    },
-    output: "static"
+  site: 'https://ourpawmise.org',
+  vite: {
+      preview: {
+          allowedHosts: ['localhost'],
+      }
+  },
+
+  output: "static",
+  integrations: [sitemap()]
 });
